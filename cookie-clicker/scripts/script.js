dceab3249ec427a84114e3ae684babafd47ba738
LukaGerh <lukagerhards@gmail.com>
2026-09-02 19:16:55 +0300

    Add files via upload

---
+++ cookie-clicker/scripts/script.js
@@ -0,0 +1,5 @@
+let switchOff = document.getElementById("switchOff")
+let switchOn = document.getElementById("switchOff")
+let lightOn= document.getElementById("lightOn")
+let lightOff = document.getElementById("lightOff")
+
