3a9e364613bfe29ca41fbc83f9d26da7e5722e00
LukaGerh <lukagerhards@gmail.com>
2026-09-08 22:43:25 +0300

    Pievienota neliela funkcionalitāte

--- cookie-clicker/scripts/script.js
+++ cookie-clicker/scripts/script.js
@@ -1,10 +1,11 @@
 
 
-let switchOff = document.getElementById("switchOff")
-let switchOn = document.getElementById("switchOn")
-let lightOn= document.getElementById("lightOn")
-let lightOff = document.getElementById("lightOff")
-let count_label = document.getElementById('click-count')
+const switchOff = document.getElementById("switchOff")
+const switchOn = document.getElementById("switchOn")
+const lightOn= document.getElementById("lightOn")
+const lightOff = document.getElementById("lightOff")
+const count_label = document.getElementById('click-count')
+const timerLabel = document.querySelector('#timer')
 
 let photoElements = document.querySelectorAll("photo-elements");
 let bttnWorth2 = document.getElementById('worth2')
@@ -15,33 +16,49 @@ switchOn.addEventListener("click", onBulbClick);
 
 /*bttnWorth2.addEventListener('click', checkCount500)*/
 
-let count = 490;
+let count = 2450;
 let step = 1;
+let randInt;
+
 function onBulbClick(){
-    if (count == 499){
-        const container = document.querySelector('.rectangle');
-        const newBttn = document.createElement("button");
-        container.appendChild(newBttn);
-        newBttn.textContent = "Collect 2 per 1 click";
-        newBttn.addEventListener('click', () => {step = 2; newBttn.remove()})
+    switchOn.classList.toggle('active');
+    switchOff.classList.toggle('active');
+    lightOff.classList.toggle('active');
+    lightOn.classList.toggle('active');
+    count_label.classList.toggle('active');
 
-    }
-        switchOn.classList.toggle('active');
-        switchOff.classList.toggle('active');
-        lightOff.classList.toggle('active');
-        lightOn.classList.toggle('active');
-        count_label.classList.toggle('active');
-
-    document.getElementById('click-count').innerHTML = count + step;
     count = count + step;
+    document.getElementById('click-count').innerHTML = count;
+    checkUpgrades()
 }
 
-function checkCount50(){
-    if (count >= 500){
-        bttnWorth2.remove()
-        return step = 2;
+function checkUpgrades(){
+    if (count == 500){
+        return upgrade500()
     }
-    else{
-        return console.log('You have not achieved count 500!')
+    else if(count == 2500){
+        return challangeAtRandTime()
     }
+
 }
+
+function upgrade500(){
+    const container = document.querySelector('.rectangle');
+    const newBttn = document.createElement("button");
+    container.appendChild(newBttn);
+    newBttn.textContent = "Collect 2 per 1 click";
+    newBttn.addEventListener('click', () => {step = 2; newBttn.remove()})
+}
+
+
+function challangeAtRandTime(){
+    const randInt =  Math.floor(Math.random() * 1001) + 2500;
+    
+}
+
+
+
+/*if(count !== randInt){
+        console.log('Sākās challange!')
+        setInterval(() => {for(i = 0; i<61; i++){timerLabel.innerHTML='i'}}, 1000);
+    }
