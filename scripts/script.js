14e0fe90f69bd5868916c8a978f8decea2b94e0e
LukaGerh <lukagerhards@gmail.com>
2026-09-15 02:49:14 +0300

    gandrīz pabeigts, bet uzskaras spēle.

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,68 @@
-
+//FIRST LIGHT BULB
 const switchOff = document.getElementById("switchOff");
 const switchOn = document.getElementById("switchOn");
 const lightOn= document.getElementById("lightOn");
 const lightOff = document.getElementById("lightOff");
-const count_label = document.getElementById('click-count');
-const timerText = document.querySelector('p.hiddenElement');
-const timerNum = document.querySelector('#timer');
+// ========
+
+//SHOP
 const container = document.querySelector('.rectangle');
+const bonus_container = document.getElementById('bonus-container');
+// SHOP BUTTONS
 const buyStepBttn = document.getElementById('buyStepBttn');
 const buyAutoClick = document.getElementById('buyAutoClickBttn');
-const main = document.getElementById("main-screen");
+const gambleBttn = document.getElementById('gambleBttn');
+// -------
 
 
+// AUDIO
 const turnOnSound = new Audio('../media/audio/turnOnSound.mp3');
 const turnOffSound = new Audio('../media/audio/turnOffSound.mp3');
 const lampExplosion = new Audio('../media/audio/lampExplosion.mp3');
 const buttonPress = new Audio('../media/audio/buttonPress.mp3');
 const timerStart = new Audio('../media/audio/timerStart.mp3');
 const tickingCountdown = new Audio('../media/audio/tickingCountdown.mp3');
+// ----
 
+const announcement = document.getElementById('announcement');
+const main = document.getElementById("main-screen");
+const digitalContainer = document.createElement('div');
+const count_label = document.getElementById('click-count');
+const challengeCount = document.getElementById('challengeCount');
+const timerText = document.querySelector('p.hiddenElement');
+const timerNum = document.querySelector('#timer');
 
+// LAMP
 let digitalSwitchOff;
 let digitalSwitchOn;
 let digitalLightOff;
 let digitalLightOn;
 
+// BUTTONS
+let newBttn500;
+
+//STATE CHECKERS
+is500Collected = false;
+
+
 let lightExploded;
 
 
-let count = 2490;
+let count = 490;
+
+// STEPS
 let step = 1;
-let stepBttnPrice = 1000;
-let autoClickPrice = 1000;
 let autoClickStep = 0;
-let autoClickState = true;
+
+// SHOP PRICES
+let stepBttnPrice = 500;
+let autoClickPrice = 800;
+let multiplierPrice = 15000;
+let gambleAmount = 100;
+
+
 let randInt = Math.floor(Math.random() * 1001) + 2500;
-let randIntTriggered = false; // Nomainīt atpakal uz false
+let randIntTriggered = false; 
 console.log("Mērķis:", randInt);
 
 
@@ -44,99 +71,89 @@ let explodeTriggered = true;
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+// EVENT LISTENERS
 buyStepBttn.addEventListener('click',()=>{shopBttns(buyStepBttn)});
 buyAutoClick.addEventListener('click', ()=>{shopBttns(buyAutoClick)});
-/*
+gambleBttn.addEventListener('click', () => { shopBttns(gambleBttn) });
+
+
 switchOff.addEventListener("click", ()=>{onBulbClick(switchOff)});
 switchOn.addEventListener("click", ()=>onBulbClick(switchOn));
-*/
-
-// Testēšanas zona
 
 
-
-//newBttn.remove();
-function test10000(){
-    switchOff.remove();
-    lightOff.remove();
-    switchOn.remove();
-    lightOn.remove();
-
-    digitalSwitchOff = document.createElement('img');
-    digitalSwitchOff.src = "../media/digitalSwitchOff.png";
-    digitalSwitchOff.alt = "digital switch for lamp which state is off";
-    digitalSwitchOff.classList.add('active');
-    main.appendChild(digitalSwitchOff);
-
-    digitalSwitchOn = document.createElement('img');
-    digitalSwitchOn.src = "../media/digitalSwitchOn.png";
-    digitalSwitchOn.alt = "digital switch for lamp which state is on";
-    //digitalSwitchOn.classList.add('active');
-    main.appendChild(digitalSwitchOn);
-
-    digitalLightOff= document.createElement('img');
-    digitalLightOff.src = "../media/digitalLightOff.png";
-    digitalLightOff.alt = "big lamp with state turned off";
-    digitalLightOff.classList.add('active');
-    main.appendChild(digitalLightOff);
-
-    digitalLightOn = document.createElement('img');
-    digitalLightOn.src = "../media/digitalLightOn.png";
-    digitalLightOn.alt = "big lamp with state turned on";
-    //digitalLightOn.classList.add('active');
-    main.appendChild(digitalLightOn);
-
-        
-
-    step = step * 4;
-
-    digitalSwitchOff.addEventListener('click', ()=>{onDigitalScroll(digitalSwitchOff)});
-    digitalSwitchOn.addEventListener('click', ()=>{onDigitalScroll(digitalSwitchOn)});
-    digitalSwitchOff.addEventListener('wheel', ()=>{onDigitalScroll(digitalSwitchOff)});
-    digitalSwitchOn.addEventListener('wheel', ()=>{onDigitalScroll(digitalSwitchOn)});
-}
-
-test10000();
-// Šeit beidzas testēšanas zona
-
+setInterval(function () {
+    if (autoClickStep > 0) {
+        count = count + autoClickStep;
+        count_label.innerHTML = count;
+    }
+}, 1000);
 
 function shopBttns(bttn){
     
     switch(bttn){
         case buyStepBttn:
             if(count>=stepBttnPrice){
+                if(is500Collected == false){
+                newBttn500.remove();
+                step=step + 1;
+                }
                 count=count-stepBttnPrice;
                 stepBttnPrice= stepBttnPrice * 2;
                 step=step * 2;
-                document.getElementById('click-count').innerHTML = count;
-                buyStepBttn.innerHTML = `Buy ${step} step for ${stepBttnPrice}`;
+                count_label.innerHTML = count;
+                buyStepBttn.innerHTML = `Add ${step} steps for ${stepBttnPrice}`;
             }
             break;
         case buyAutoClick: 
             if(count>=autoClickPrice){
-                count.innerHTML = count-autoClickPrice;
                 count=count-autoClickPrice;
-                autoClickFunction();
+                count_label.innerHTML = count;
+                autoClickPrice = autoClickPrice * 1.5;
+                if(autoClickStep == 0){
+                    autoClickStep = autoClickStep + 1;
+                }
+                else{
+                    autoClickStep = autoClickStep * 2;
+                }
+                buyAutoClick.innerHTML = `Add additional ${autoClickStep} autoclicks for ${autoClickPrice}`;
             }
-            break; 
+            break;
+        case gambleBttn:
+            if (count >= gambleAmount) {
+                let isWin = Math.random() < 0.5;
+
+                if (isWin) {
+                    count = count + gambleAmount;
+                    announcement.innerHTML = `You won ${gambleAmount} clicks!`;
+                } else {
+                    count = count - gambleAmount;
+                    announcement.innerHTML = `You lost ${gambleAmount} clicks!`;
+                }
+                
+               
+                gambleAmount = Math.round(gambleAmount * 1.5); 
+                gambleBttn.innerHTML = `Risk with ${gambleAmount} clicks`;
+
+                count_label.innerHTML = count;
+                checkUpgrades();
+            } 
+            else {
+                announcement.innerHTML = "Tev nepietiek punktu, lai riskētu!";
+            }
+            setTimeout(() => {announcement.innerHTML = '';}, 3000);
+            break;
     }
 }
 
-function autoClickFunction(){
-
-    while(autoClickState == true){
-        count.innerHTML = count + autoClickStep;
-        count = count + autoClickStep; 
-    }
-    
-}
 
 function onBulbClick(switchPressed){
     if(switchPressed == switchOff){
-        turnOnSound.play().catch(e => console.log("Nevar atskaņot skaņu:", e));;
+        turnOnSound.currentTime = 0; // Pārtrauc iepriekšējo un sāk no jauna
+        turnOnSound.play().catch(e => console.log(e));
     }
     else if(switchPressed == switchOn){
-        turnOffSound.play().catch(e => console.log("Nevar atskaņot skaņu:", e));;
+        turnOffSound.currentTime = 0; // Pārtrauc iepriekšējo un sāk no jauna
+        turnOffSound.play().catch(e => console.log(e));
     }
     switchOn.classList.toggle('active');
     switchOff.classList.toggle('active');
@@ -145,7 +162,7 @@ function onBulbClick(switchPressed){
     count_label.classList.toggle('active');
 
     count = count + step;
-    document.getElementById('click-count').innerHTML = count;
+    count_label.innerHTML = count;
     checkUpgrades();
 }
 
@@ -171,40 +188,49 @@ function onDigitalScroll(digSwitchPressed){
 
 
 async function checkUpgrades(){
-    if (count == 500){
+    if (count == 500 && !is500Collected){
         upgrade500();
     }
     else if (count >= randInt && !randIntTriggered) {
         randIntTriggered = true;
-        
         startChallenge(); 
     }
     else if (count >= explodeTime){
-        //explodeTriggered = true;
         removeEventListener('wheel', onDigitalScroll)
         lightBulbExplodes();
     }
 }
 
 function upgrade500(){
-    
-    const newBttn = document.createElement("button");
-    container.appendChild(newBttn);
-    newBttn.textContent = "Collect 2 per 1 click";
-    newBttn.addEventListener('click', () => {step = 2; newBttn.remove()})
+    newBttn500 = document.createElement("button");
+    bonus_container.appendChild(newBttn500);
+    newBttn500.textContent = "Collect 2 per 1 click";
+    newBttn500.addEventListener('click', () => {step = 2; is500Collected = true; newBttn500.remove();})
 }
 
 
 
 async function startChallenge(){
     let timerCount = 60;
-    let startCount = count;
+    //let startCount = count;
     let newBttn;
+    announcement.innerHTML = 'Get atleast 400 clicks for instant bonus!<br> (clicks are counted by 1)';
     timerText.classList.toggle('active');
-    timerStart.play();
 
+    let clicksCounted = 0;
+    
+    timerStart.play();
+   
     await sleep(2000);
     tickingCountdown.play();
+
+    switchOff.addEventListener('click', countClicks);
+    switchOn.addEventListener('click', countClicks);
+     function countClicks(){
+        clicksCounted = clicksCounted + 1;
+        challengeCount.innerHTML = clicksCounted;
+    }
+
     for(let i = 0; i < 61; i++){
 
         timerNum.innerHTML = timerCount;
@@ -212,19 +238,21 @@ async function startChallenge(){
         await sleep(1000);
     }
 
+    announcement.innerHTML ='';
+    challengeCount.remove();
     newBttn = document.createElement('button');
     newBttn.textContent = "Collect smart switch";
 
-    if(count-startCount >= 400){
+    if(clicksCounted >= 400){
         timerText.classList.toggle('active');
-        console.log('Klikšķi 60 sekundēs', count-startCount);
+        console.log('Klikšķi 60 sekundēs', clicksCounted);
         container.appendChild(newBttn);
         
     }   
     else{
         timerText.classList.toggle('active');
-        console.log('Klikšķi 60 sekundēs', count-startCount);
-        await sleep(15000);
+        console.log('Klikšķi 60 sekundēs', clicksCounted);
+        await sleep(60000);
         container.appendChild(newBttn);
     }
     newBttn.addEventListener('click', function(){
@@ -234,33 +262,40 @@ async function startChallenge(){
         switchOn.remove();
         lightOn.remove();
 
+        digitalContainer.classList.add('digitalContainer')
+        main.appendChild(digitalContainer);
+
         digitalSwitchOff = document.createElement('img');
         digitalSwitchOff.src = "../media/digitalSwitchOff.png";
         digitalSwitchOff.alt = "digital switch for lamp which state is off";
-        digitalSwitchOff.classList.add('active');
-        main.appendChild(digitalSwitchOff);
+        //digitalSwitchOff.classList.add('active');
+        //digitalSwitchOff.classList.add('lamp');
+        digitalContainer.appendChild(digitalSwitchOff);
 
         digitalSwitchOn = document.createElement('img');
         digitalSwitchOn.src = "../media/digitalSwitchOn.png";
         digitalSwitchOn.alt = "digital switch for lamp which state is on";
-        //digitalSwitchOn.classList.add('active');
-        main.appendChild(digitalSwitchOn);
+        digitalSwitchOn.classList.add('active');
+        //digitalSwitchOn.classList.add('lamp');
+        digitalContainer.appendChild(digitalSwitchOn);
 
         digitalLightOff= document.createElement('img');
         digitalLightOff.src = "../media/digitalLightOff.png";
         digitalLightOff.alt = "big lamp with state turned off";
-        digitalLightOff.classList.add('active');
-        main.appendChild(digitalLightOff);
+        //digitalLightOff.classList.add('active');
+        //digitalLightOff.classList.add('lamp');
+        digitalContainer.appendChild(digitalLightOff);
 
         digitalLightOn = document.createElement('img');
         digitalLightOn.src = "../media/digitalLightOn.png";
         digitalLightOn.alt = "big lamp with state turned on";
-        //digitalLightOn.classList.add('active');
-        main.appendChild(digitalLightOn);
+        digitalLightOn.classList.add('active');
+        //digitalLightOn.classList.add('lamp');
+        digitalContainer.appendChild(digitalLightOn);
 
         
 
-        step = step * 2;
+        //step = step * 2;
         digitalSwitchOff.addEventListener('click', ()=>{onDigitalScroll(digitalSwitchOff)});
         digitalSwitchOn.addEventListener('click', ()=>{onDigitalScroll(digitalSwitchOn)});
         digitalSwitchOff.addEventListener('wheel', ()=>{onDigitalScroll(digitalSwitchOff)});
@@ -301,10 +336,10 @@ function lightBulbExplodes(){
         count = Math.floor(count - (count/5));
         count_label.innerHTML = count;
         lightExploded.remove();
-        main.appendChild(digitalSwitchOff);
-        main.appendChild(digitalSwitchOn);
-        main.appendChild(digitalLightOff);
-        main.appendChild(digitalLightOn);
+        digitalContainer.appendChild(digitalSwitchOff);
+        digitalContainer.appendChild(digitalSwitchOn);
+        digitalContainer.appendChild(digitalLightOff);
+        digitalContainer.appendChild(digitalLightOn);
         newBttn.remove();
     
     }
